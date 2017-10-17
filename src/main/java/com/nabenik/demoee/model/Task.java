aaa3eb7317bff33710a882513e50fb24fcf95f3a
Víctor Orozco <vorozco@nabenik.com>
2017-10-17 11:48:32 -0500

    Adding persistence task

---
+++ src/main/java/com/nabenik/demoee/model/Task.java
@@ -0,0 +1,107 @@
+package com.nabenik.demoee.model;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Version;
+
+@Entity
+public class Task implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, nullable = false)
+	private Long id;
+	@Version
+	@Column(name = "version")
+	private int version;
+
+	@Column(name = "task_name", nullable = false)
+	private String taskName;
+
+	@Column(length = 2000, nullable = false)
+	private String description;
+
+	@Column
+	private boolean finished;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(final Long id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return this.version;
+	}
+
+	public void setVersion(final int version) {
+		this.version = version;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Task)) {
+			return false;
+		}
+		Task other = (Task) obj;
+		if (id != null) {
+			if (!id.equals(other.id)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	public String getTaskName() {
+		return taskName;
+	}
+
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isFinished() {
+		return finished;
+	}
+
+	public void setFinished(boolean finished) {
+		this.finished = finished;
+	}
+
+	@Override
+	public String toString() {
+		String result = getClass().getSimpleName() + " ";
+		if (taskName != null && !taskName.trim().isEmpty())
+			result += "taskName: " + taskName;
+		if (description != null && !description.trim().isEmpty())
+			result += ", description: " + description;
+		result += ", finished: " + finished;
+		return result;
+	}
+}
